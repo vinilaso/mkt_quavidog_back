b127390576b4c9b1f637548019a9f1e076a32013
Vinicius Lanser <vini.lanser@gmail.com>
2026-03-28 22:51:55 -0300

    Detalhamento das APIs

--- Sienna.WebApi/Controllers/Identity/IdentityController.cs
+++ Sienna.WebApi/Controllers/Identity/IdentityController.cs
@@ -7,11 +7,14 @@ using Sienna.WebApi.Contracts.Identity;
 
 namespace Sienna.WebApi.Controllers.Identity
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class IdentityController(IMediator mediator) : SiennaController
     {
         [HttpPost("create")]
+        [ProducesResponseType<Guid>(StatusCodes.Status201Created, Description = "O usuário foi criado com sucesso no banco de dados.")]
+        [ProducesResponseType<ProblemDetails>(StatusCodes.Status400BadRequest, Description = "Falha de validação nos parâmetros de entrada.")]
+        [ProducesResponseType<ProblemDetails>(StatusCodes.Status409Conflict, Description = "Conflito de estado, como e-mail já registrado.")]
         public async Task<IActionResult> CreateUserAsync([FromBody] RegisterUserCommand request)
         {
             Result<Guid> result = await mediator.Send(request);
@@ -19,10 +22,12 @@ namespace Sienna.WebApi.Controllers.Identity
             if (result.IsFailure)
                 return HandleFailure(result);
 
-            return Created("create", result.Value);
+            return Created(string.Empty, result.Value);
         }
 
         [HttpPost("login")]
+        [ProducesResponseType<LoginResponse>(StatusCodes.Status200OK, Description = "O usuário com e-mail e senha informados foi encontrado e o token JWT foi gerado com sucesso.")]
+        [ProducesResponseType<ProblemDetails>(StatusCodes.Status401Unauthorized, Description = "Não foi encontrado usuário com e-mail e senha informados, ou o usuário está bloqueado.")]
         public async Task<IActionResult> GetTokenAsync([FromBody] LoginCommand request)
         {
             var result = await mediator.Send(request);
